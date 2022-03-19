6f27e2576a4386b7a86b2f286f93415e15256fca
Marcos <marcos_fmendes@hotmail.com>
2022-03-19 13:41:55 -0300

    Comentário debugged

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router";
+import { useContext } from "react";
 import Feed from "../Feed/Feed";
 import UserHeader from "./UserHeader";
 import UserPhotoPost from "./UserPhotoPost";
 import UserStats from "./UserStats";
+import { UserContext } from "../../UserContext";
 
 const User = () => {
+  const { data } = useContext(UserContext);
+  console.log(data);
   return (
     <section className="container">
       <UserHeader />
