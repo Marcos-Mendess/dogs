16582811ba842771fca8fc18573240e946b6dcc4
Marcos <marcos_fmendes@hotmail.com>
2022-03-21 13:10:39 -0300

    Route bug fix

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
           <Header />
           <main className="AppBody">
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/" element={<Login />} />
               <Route path="login/*" element={<Login />} />
               <Route
                 path="conta/*"
